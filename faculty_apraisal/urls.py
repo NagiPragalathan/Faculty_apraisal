4867626b602af3871efee09247db6db5f9dcc47c
Hari-7822 <h26058549@gmail.com>
2023-01-15 01:07:13 +0530

    login

--- faculty_apraisal/urls.py
+++ faculty_apraisal/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('login',views.home)
 ]
---
+++ static/js/toggle.js
@@ -0,0 +1,9 @@
+
+var pass = () => {
+
+    var input = document.querySelector('.mail__form--pass-ip')
+    var eye = document.querySelector('.eye')
+
+    eye.className === "fa-eye" ? "fa-eye-slash" : "fa-eye"
+
+}
