c45aae2f848acbda5ed1667e01d0b587632ef12c
Hari-7822 <h26058549@gmail.com>
2023-01-16 14:03:36 +0530

    script

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -1,9 +1,12 @@
 
-var pass = () => {
+function pass () {
 
     var input = document.querySelector('.mail__form--pass-ip')
     var eye = document.querySelector('.eye')
 
-    eye.className === "fa-eye" ? "fa-eye-slash" : "fa-eye"
+    input.type === "password" ? "text" : "password"
+
+
+    // eye.className === "fa-eye" ? "fa-eye-slash" : "fa-eye"
 
 }
